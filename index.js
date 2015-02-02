fdee359ad3b6d1c50b1c1f329b9f5f23b4eb1baa
fereidani <info@fereidani.com>
2015-02-03 01:16:14 +0330

    Release

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
 /**
- * Created by ircrash on 2/2/15.
+ * Created by Khashayar Fereidani on 2/2/15.
  */
+"use strict";
+var session=require('express-session');
+var store=require('connect-redis')(session);
+
+
+// NOTICE : DO NOT EDIT THESE SETTINGS DIRECTLY , SET YOUR DESIRED VALUE IN YOUR CONFIG FILE.
+
+var finalSessionSettings={
+        "name":"skyport",
+        "secret":"hello world!!"
+    };
+
+var finalRedisSettings={
+    "db":0,
+    "prefix":"sess:"
+    };
+
+var settings={
+    defaultError:"Redis Connection Timeout/Problem !"
+    };
+
+
+function cp(from,to){
+    if(from!=undefined && from instanceof Object){
+        for(var index in from){
+            to[index]=from[index];
+            console.log(to);
+        }
+    }
+}
+
+module.exports=function(sessionSettings,redisSettings,middlewareSettings) {
+    cp(middlewareSettings,settings)
+    cp(sessionSettings, finalSessionSettings);
+    cp(redisSettings, finalRedisSettings);
+    console.log(finalRedisSettings);
+    finalSessionSettings.store = new store(finalRedisSettings);
+    var sessionHandler = session(finalSessionSettings);
+    return function (req, res, next) {
+        sessionHandler(req, res, function () {
+            if (!req.session) {
+                return next(new Error(settings.defaultError));// throw error
+            }else {
+                next();// no error continue
+            }
+        });
+    }
+}
